d703b15bf2ff8b0065e4bcbd98dd3537db48885a
Shameoff <eshamov030316@gmail.com>
2022-11-27 04:10:35 +0700

    Добавил всякие мелочи

--- src/components/DishMini.js
+++ src/components/DishMini.js
@@ -3,13 +3,14 @@ import React from "react";
 
 function DishMini(props){
     return (
-        <div>
+        <div className='foodItem'>
             <img src={props.item.image}/>
             <h2>{props.item.name}</h2>
             <p>Категория блюда - {props.item.category}</p>
             <p>{props.item.description}</p>
-            <p>{props.item.price}</p>
+            <p>{props.item.price} &#8381;</p>
             <p>isVegeterian? {`${props.item["vegetarian"]}`}</p>
+            <div className="add-to-basket">+</div>
         </div>
     )// Почему веганская ли еда показывается только таким костылем ??
 }
--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -3,9 +3,9 @@ import DishMini from "./DishMini";
 
 function Dishes(props) {
     return (
-        <>
+        <div className='food-container'>
             {props.items.map((item, key) => <DishMini item={item} key={key}/>)}
-        </>
+        </div>
     )
 }
 
