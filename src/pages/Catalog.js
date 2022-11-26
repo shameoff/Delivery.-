35d1de462dacb18f06b6cc403a95e25af56b2788
Shameoff <eshamov030316@gmail.com>
2022-11-26 09:26:15 +0700

    Незаконченный коммит

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,6 +1,16 @@
 import React from "react";
+import axios from "axios";
+
+async function getData(){
+    let URL = process.env.REACT_APP_API_URL
+    return await axios.get(`${URL}/api/dish`)
+        .then(function (response){
+            console.log(response)
+        })
+}
 
 function Catalog(props){
+    console.log(getData())
     return (
         <></>
     )
