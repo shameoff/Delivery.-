fb38405a24c2bc01648b1da9849ce563dbc27b81
Egor Shamov <eshamov030316@gmail.com>
2022-11-28 07:36:02 +0700

    Теперь NotFound показывает navbar. Попробовал добавить бустрап в navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,19 @@
 import {Outlet} from "react-router-dom";
 import NaviBar from "./NaviBar";
 import Footer from "./Footer";
+import {Container} from "react-bootstrap";
 
 
 function Layout() {
-    return(
-        <>
-        <NaviBar />
+    return (
+        <Container className="p-3">
+            <NaviBar/>
 
-        <Outlet />
+            <Outlet/>
 
-        <Footer />
-        </>
+            <Footer/>
+        </Container>
     )
 }
+
 export default Layout
