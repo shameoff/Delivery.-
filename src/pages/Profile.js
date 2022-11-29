1cc3f531a79c464c7be929d5c83e198b2ac648cf
Egor Shamov <eshamov030316@gmail.com>
2022-11-30 02:23:19 +0700

    Добавил заглушки для страницы профиля

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+
+function Profile(props) {
+    return (
+        <>
+            <h1>Мой профиль: </h1>
+            <ProfileCard/>
+        </>
+    )
+}
+
+export default Profile;
