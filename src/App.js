ae04209355c0c73ce1ee5ddaf3d575b7885a2ec2
Shameoff <eshamov030316@gmail.com>
2022-12-01 11:35:19 +0700

    Обновил роуты. Добавил всё, что там должно быть

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import {Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout"
 import Registration from "./pages/Registration";
@@ -6,7 +7,10 @@ import Basket from "./pages/Basket";
 import Catalog from "./pages/Catalog";
 import NotFound from "./pages/NotFound";
 import ApiResponse from "./components/ApiResponse";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Profile from "./pages/Profile";
+import MyOrders from "./pages/MyOrders";
+import Dish from "./pages/Dish";
+import Order from "./pages/Order";
 
 function App() {
     return (
@@ -15,8 +19,13 @@ function App() {
                 <Route path="/api" element={<ApiResponse/>}/>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Catalog/>}/>
-                    <Route path="registration" element={<Registration/>}/>
                     <Route path="basket" element={<Basket/>}/>
+                    <Route path="menu/:id" element={<Dish/>}/>
+                    <Route path="menu" element={<Catalog/>}/>
+                    <Route path="order/:id" element={<Order/>}/>
+                    <Route path="orders" element={<MyOrders/>}/>
+                    <Route path="profile" element={<Profile/>}/>
+                    <Route path="registration" element={<Registration/>}/>
                     <Route path="*" element={<NotFound/>}/>
                 </Route>
             </Routes>
