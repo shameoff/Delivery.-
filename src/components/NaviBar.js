52cc5e349cc43fcf24d9985101c5d5be0e86d9cb
Egor Shamov <eshamov030316@gmail.com>
2022-12-15 04:58:47 +0700

    Прикрутил счетчик корзины, который работает при обновлении страницы

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -8,8 +8,9 @@ function NaviBar(props) {
 
     const [authorizedEmail, setAuthorizedEmail] = useState(false)
     const navigate = useNavigate()
-
+    const [basketAmount, setBasketAmount] = useState(false)
     useEffect(() => {
+
         getPrivateData("/account/profile")
             .catch((error) => {
                 switch (error.status) {
@@ -22,38 +23,53 @@ function NaviBar(props) {
             .then(resp => {
                 setAuthorizedEmail(resp.data.email)
             })
+
+        getPrivateData("/basket")
+            .catch((error) => {
+                switch (error.status) {
+                    case 401:
+                        console.log("Не авторизован для получения данных корзины")
+                        setBasketAmount(false)
+                        break
+                }
+            })
+            .then(resp => {
+                setBasketAmount(resp.data.map(item => item.amount).reduce((prev, next) => prev + next))
+            })
     }, [])
-    useEffect(() => {
-        if (authorizedEmail) navigate("/")
-    }, [authorizedEmail])
 
     return (
         <>
             <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' className="rounded-2 ps-2 pe-2">
-                <Navbar.Brand>Delivery.Кушац</Navbar.Brand>
+                <Navbar.Brand href="/">Delivery.Кушац</Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
-                        <Nav.Link href="/">Меню</Nav.Link>
-                        <Nav.Link href="/orders">Заказы</Nav.Link>
-                        <Nav.Link href="/basket">Корзина</Nav.Link>
+                        <Nav.Link as={Link} to="/">Меню</Nav.Link>
+                        <Nav.Link as={Link} to="/orders">Заказы</Nav.Link>
+                        <Nav.Link as={Link} to="/cart">
+                            <div className="d-flex flex-row">Корзина &nbsp;
+                                {basketAmount && <div className="bg-success w-auto rounded">{` ${basketAmount} `}</div>}
+                            </div>
+                        </Nav.Link>
                     </Nav>
                     <Nav className="ms-auto">
                         {authorizedEmail && <>
-                            <Nav.Link href="/profile">{authorizedEmail}</Nav.Link>
+                            <Nav.Link as={Link} to="/profile">{authorizedEmail}</Nav.Link>
                             <Button variant='primary' className="ms-2 mb-1" onClick={event => {
                                 logout().then(() => {
-                                        navigate("/")
-                                        setAuthorizedEmail(false)
-                                    })
+                                    navigate("/")
+                                    setAuthorizedEmail(false)
+                                })
                             }
                             }>Log Out</Button>
                         </>}
                         {!authorizedEmail && <>
-                            <Nav.Link href="/login"><Button variant='primary' className="ms-2 mb-1">Log
+                            <Nav.Link as={Link} to="/login"><Button variant='primary' className="ms-2 mb-1">Log
                                 In</Button></Nav.Link>
-                            <Nav.Link href="/registration"><Button variant='primary' className="ms-2 mb-1">Sign In</Button></Nav.Link>
+                            <Nav.Link as={Link} to="/registration"><Button variant='primary' className="ms-2 mb-1">Sign
+                                In</Button></Nav.Link>
                         </>}
                     </Nav>
                 </Navbar.Collapse>
